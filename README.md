8a2acdcda0911a31c54262a18e3fc2cb7c34c78b
Bami85 <91563447+Bami85@users.noreply.github.com>
2022-05-27 01:02:37 +0200

    sista ändringar

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Twisted-Stories
+Beskrivning
+
+Det här är ett textbaserat-spel uppbyggt på TypeScript och genererad via Vite.Js. Och Under spelet för en spelare alltid  välja mellan två alternativt, och på den här sättet spelaren kan styra själva spelet.
+
+Krav som ska uppfyllas för godkänt:
+
+Git & GitHub har använts ✅
+Projektmappen innehåller en README.md fil ✅
+Uppgiften lämnas in i tid! ✅
+Muntlig presentation är genomförd ❎❌
+Applikationens struktur skall vara genomtänk samt uppdelad i olika moduler. ✅
+Applikationen skall vara skriven i Typescript samt genererad via VITE. Ts-config som generaras via VITE skall inte ändras för att ta bort typescript-regler. ✅
+Inga ”any”-typningar får finnas. ✅
+Minst 1 st. Interface skall användas ✅
+Spelet skall ha minst 5 vägskäl/frågor med minst 2 möjliga svar per fråga (alla vägskäl/frågor behöver inte visas för att klara spelet). ✅
+Interaktionen ifrån användaren skall ske via ett gränssnitt med knappar som svar på frågorna. ✅
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,55 @@
 import './style.css'
+import { gameLivell } from './gameData'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+let text = document.querySelector<HTMLDivElement>('#text')! //hämta question div
+let leftBtn = document.getElementById("leftBtn") as HTMLElement //hämtar vänster knapp
+let rightBtn = document.getElementById("rightBtn") as HTMLElement //hämtar vänster knapp
+//test
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+
+function onLoad(): void {  
+  renderStep()
+}
+
+let currentGamestep = gameLivell[0]
+
+
+
+function gameprogress(this: HTMLElement, _event: MouseEvent): void {
+  let nextStep: number = 0  
+  if (this.id == "leftBtn") {
+    nextStep = currentGamestep.choises.leftBtn!.path
+    
+  } else if(this.id == "rightBtn") {
+    nextStep = currentGamestep.choises.rightBtn!.path
+  } else(
+    console.log("crash")
+    )
+    
+    const found = gameLivell.find(function(gameStep) {
+      return gameStep.id == nextStep
+    });
+    if ( found ) {
+      currentGamestep = found
+      console.log(found)
+      renderStep()
+    }
+    
+  }  
+  
+  function renderStep() {
+    text!.innerText = currentGamestep.question
+    leftBtn!.innerText = currentGamestep.choises.leftBtn!.answer
+    rightBtn!.innerText = currentGamestep.choises.rightBtn!.answer
+  }
+   
+   
+  leftBtn!.addEventListener("click", gameprogress)  
+
+  rightBtn!.addEventListener("click", gameprogress) 
+
+
+onLoad()
+
+
